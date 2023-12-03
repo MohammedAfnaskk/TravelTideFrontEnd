04de1b54c707ff487dd6953a88a9d217dd80dee4
Mohammed Afnas <afnaskkvlr@gmail.com>
2023-12-03 21:49:10 +0530

    vercel.json create

--- src/components/Payment/payment.jsx
+++ src/components/Payment/payment.jsx
@@ -1,46 +1,83 @@
-import React, { useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { UserUrl } from '../../constants/constants';
-import axios from 'axios'; // Add this import statement
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
+import { UserUrl } from "../../constants/constants";
+import axios from "axios"; // Add this import statement
+import jwt_decode from "jwt-decode";
+import { userAxiosInstant } from "../../utils/axiosUtils";
 
 const HomePages = () => {
-	const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const decode = jwt_decode(token);
+  const user_id = decode.user_id;
+  const updatePaymentStatus = async () => {
+    try {
+      const currentDate = new Date().toISOString();
 
-	const SubmitPayment = async () => {
-		try {
-		  const response = await axios.post(`${UserUrl}/payments/create-checkout-session`, {
-			// Include any necessary data in the request payload
-			// dataKey: dataValue,
-		  }, {
-			withCredentials: true, // Set to true if needed
-		  });
-	  
-		  console.log("Checkout URL:", response.data.checkout_url);
-	  
- 		  window.location.href = response.data.message.url
+      const res = await userAxiosInstant.patch(
+        `payments/payment/${user_id}/`,
+        {
+          payment: true,
+          payment_date: currentDate,
+        },
+        {
+          withCredentials: true,
+        }
+      );
 
-		  // Redirect the user to the Stripe Checkout page
-		//   window.location.href = `https://checkout.stripe.com/pay/cs_test_a1i1GDq4cc6bt5zYhwfqZlbhXyCFmx0xz7KVGouG3P3mVGRRMpcdlFegQ1#fidkdWxOYHwnPyd1blpxYHZxWjA0SjBKU1RWTWloS1VEY1ZPR1QwS0RHSFBKQ31GND1DRGwxSUtyV189UnxHQWdualxUYl9MbnFwPGRgcHc0R2o8SExsUHxwQTBCPF1RXzdCT1NdXVVKSGxtNTVwdUpDSHJwaCcpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl`;
-		} catch (error) {
-		  console.error("Error sending payment:", error);
-		  toast.error("Error sending Payment");
+      console.log("Axios Response:", res.data);
+      toast.success("Payment successful");
+    } catch (error) {
+      console.error("Error updating payment status:", error);
+      toast.error("Error updating payment status");
+    }
+  };
+
+  const SubmitPayment = async () => {
+    try {
+      const response = await axios.post(
+        `${UserUrl}/payments/create-checkout-session`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+ 
+      console.log("Checkout URL:", response.data.checkout_url);
+
+      window.location.href = response.data.checkout_url;
+
+	  setTimeout(() => {
+ 		const success = new URLSearchParams(window.location.search).get("success");
+		if (success === "true") {
+ 		  updatePaymentStatus();
+		} else {
+ 		  const canceled = new URLSearchParams(window.location.search).get("canceled");
+		  if (canceled === "true") {
+			console.log("Payment canceled");
+ 		  }
 		}
-	  };
+	  }, 3000); 
+	  
+    } catch (error) {
+      console.error("Error sending payment:", error);
+      toast.error("Error sending Payment");
+    }
+  };
+
   return (
     <section>
-      <div className='product'>
+      <div className="product">
         <img
-          src='https://i.imgur.com/EHyR2nP.png'
-          alt='The cover of Stubborn Attachments'
+          src="https://i.imgur.com/EHyR2nP.png"
+          alt="The cover of Stubborn Attachments"
         />
-        <div className='description'>
+        <div className="description">
           <h3>Stubborn Attachments</h3>
           <h5>$20.00</h5>
         </div>
       </div>
 
-      <button className='button' onClick={SubmitPayment}>
+      <button className="button" onClick={SubmitPayment}>
         Checkout
       </button>
     </section>
